8c9f1c3dc543bf71a2217cad5d0838c56ab0bd49
nxh1811 <85869410+nxh1811@users.noreply.github.com>
2026-07-09 17:56:35 +0700

    Force fresh bulk downloader bundle

--- registerSW.js
+++ registerSW.js
@@ -1,6 +1,6 @@
 (() => {
     const resetKey = 'xh-aio-cache-reset-version';
-    const resetVersion = 'bulk-module-20260709-9';
+    const resetVersion = 'bulk-module-20260709-10';
 
     window.addEventListener('load', async () => {
         const needsReload = window.localStorage.getItem(resetKey) !== resetVersion;
